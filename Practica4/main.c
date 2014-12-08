ccb26dd6a4d0bc33364269459f4f87a945cae740
DanielAguado <danielaguadoaraujo@gmail.com>
2014-12-08 15:06:15 +0100

    Detecta los dos patrones y los dibuja bien. Ahora mismo el segundo patron dibuja una tetera

--- Practica4/main.c
+++ Practica4/main.c
@@ -4,12 +4,48 @@
 #include <AR/param.h>   
 #include <AR/ar.h>
 #include <AR/arMulti.h>
-
+//06_solucion_dibujo3d
 // ==== Definicion de constantes y variables globales ===============
 ARMultiMarkerInfoT  *mMarker;       // Estructura global Multimarca
+
+int multipatt_id;
+
+// ==== Definicion de estructuras ===================================
+struct TObject{
+  int id;                      // Identificador del patron
+  int visible;                 // Es visible el objeto?
+  double width;                // Ancho del patron
+  double center[2];            // Centro del patron  
+  double patt_trans[3][4];     // Matriz asociada al patron
+  void (* drawme)(void);       // Puntero a funcion drawme
+};
+
+int nobjects = 0;
+struct TObject *objects = NULL;
+
 int dmode = 0;   // Modo dibujo (objeto centrado o cubos en marcas)
 
+// ==== addObject (Anade objeto a la lista de objetos) ==============
+void addObject(char *p, double w, double c[2], void (*drawme)(void)) 
+{
+  int pattid;
+
+  if((pattid=arLoadPatt(p)) < 0) 
+    print_error ("Error en carga de patron\n");
+
+  nobjects++;
+  objects = (struct TObject *) 
+    realloc(objects, sizeof(struct TObject)*nobjects);
+
+  objects[nobjects-1].id = pattid;
+  objects[nobjects-1].width = w;
+  objects[nobjects-1].center[0] = c[0];
+  objects[nobjects-1].center[1] = c[1];
+  objects[nobjects-1].drawme = drawme;
+}
+
 void print_error (char *error) {  printf("%s\n",error); exit(0); }
+
 // ======== cleanup =================================================
 static void cleanup(void) {   // Libera recursos al salir ...
   arVideoCapStop();   arVideoClose();   argCleanup();   exit(0);
@@ -24,10 +60,7 @@ static void keyboard(unsigned char key, int x, int y) {
 }
 
 
-void drawline (float w, float r, float g, float b, 
-                      float x1, float y1, float z1, 
-                      float x2, float y2, float z2) {
-
+void drawline (float w, float r, float g, float b, float x1, float y1, float z1, float x2, float y2, float z2) {
   glColor3f(r, g, b);
   glLineWidth(w);
   glBegin(GL_LINES);
@@ -35,7 +68,6 @@ void drawline (float w, float r, float g, float b,
     glVertex3f(x2, y2, z2);
   glEnd();
 }
-
 
 void drawQuad () {
   glColor3f(1, 1, 1);
@@ -50,6 +82,16 @@ void drawQuad () {
   drawline(5, 0, 1, 0, 0, 0, 0, 0, -210, 0);
   drawline(5, 0, 0, 1, 0, 0, 0, 0, 0, 210);
 }
+
+// ==== draw****** (Dibujado especifico de cada objeto) =============
+void drawteapot(void) {
+  GLfloat material[]     = {0.0, 0.0, 1.0, 1.0};
+  glMaterialfv(GL_FRONT, GL_AMBIENT, material);
+  glTranslatef(0.0, 0.0, 60.0);
+  glRotatef(90.0, 1.0, 0.0, 0.0);
+  glutSolidTeapot(80.0);
+}
+
 // ======== draw ====================================================
 static void draw( void ) {
   double  gl_para[16];   // Esta matriz 4x4 es la usada por OpenGL
@@ -65,6 +107,20 @@ static void draw( void ) {
   glLoadMatrixd(gl_para);               
 
   drawQuad();  
+  
+  int i;
+  for (i=0; i<nobjects; i++) {
+    if (objects[i].visible) {   // Si el objeto es visible
+      argConvGlpara(objects[i].patt_trans, gl_para);   
+      glMatrixMode(GL_MODELVIEW);           
+      glLoadMatrixd(gl_para);   // Cargamos su matriz de transf.            
+/*
+      glEnable(GL_LIGHTING);  glEnable(GL_LIGHT0);
+      glLightfv(GL_LIGHT0, GL_POSITION, light_position);*/
+      objects[i].drawme();      // Llamamos a su función de dibujar
+    }
+  }
+
   glDisable(GL_DEPTH_TEST);
 }
 
@@ -72,7 +128,8 @@ static void draw( void ) {
 static void init( void ) {
   ARParam  wparam, cparam;   // Parametros intrinsecos de la camara
   int xsize, ysize;          // Tamano del video de camara (pixels)
-  
+  double c[2] = {0.0, 0.0};  // Centro de patron (por defecto)
+
   // Abrimos dispositivo de video
   if(arVideoOpen("-dev=/dev/video0") < 0) exit(0);  
   if(arVideoInqSize(&xsize, &ysize) < 0) exit(0);
@@ -88,6 +145,8 @@ static void init( void ) {
   if( (mMarker = arMultiReadConfigFile("data/marker.dat")) == NULL )
     print_error("Error en fichero marker.dat\n");
 
+  addObject("data/simple.patt", 120.0, c, drawteapot); 
+
   argInit(&cparam, 1.0, 0, 0, 0, 0);   // Abrimos la ventana 
 }
 
@@ -95,7 +154,7 @@ static void init( void ) {
 static void mainLoop(void) {
   ARUint8 *dataPtr;
   ARMarkerInfo *marker_info;
-  int marker_num;
+  int marker_num, i, j, k;
 
   // Capturamos un frame de la camara de video
   if((dataPtr = (ARUint8 *)arVideoGetImage()) == NULL) {
@@ -113,6 +172,21 @@ static void mainLoop(void) {
 
   arVideoCapNext();      // Frame pintado y analizado... A por otro!
   
+  for (i=0; i<nobjects; i++) {
+    for(j = 0, k = -1; j < marker_num; j++) {
+      if(objects[i].id == marker_info[j].id) {
+        if (k == -1) k = j;
+        else if(marker_info[k].cf < marker_info[j].cf) k = j;
+      }
+    }
+    
+    if(k != -1) {   // Si ha detectado el patron en algun sitio...
+      objects[i].visible = 1;
+      arGetTransMat(&marker_info[k], objects[i].center, 
+        objects[i].width, objects[i].patt_trans);
+    } else { objects[i].visible = 0; }  // El objeto no es visible
+  }
+
   if(arMultiGetTransMat(marker_info, marker_num, mMarker) > 0) 
     draw();       // Dibujamos los objetos de la escena
   
